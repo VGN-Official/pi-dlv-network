6bcda520689f29e79a61f796024597ad99e74b93
mohammedbabangida94-bit <mohammedbabangida94@gmail.com>
2026-06-26 17:41:23 +0100

    Update fix

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ const isPiBrowserEngine = (typeof Pi !== 'undefined');
 // =======================================================
 if (isPiBrowserEngine) {
     try {
-        Pi.init({ version: "2.0", sandbox: false });
+        Pi.init({ version: "2.0", sandbox: true });
         console.log("Pi SDK Matrix Initialized.");
 // 🎯 FIX: Removed the invalid 'payments' string from the array
         Pi.authenticate(['username'], onIncompletePaymentFound)
